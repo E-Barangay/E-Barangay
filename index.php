be83c5394af796fffc43ccff80b6bb7bc3b54d63
Vincent <angelesemanuelvincent@gmail.com>
2025-06-26 20:33:01 +0800

    index.php|BE

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
 
 include("sharedAssets/connect.php");
-
 session_start();
 
 ?>
+
 
 <!doctype html>
 <html lang="en">
@@ -18,122 +18,114 @@ session_start();
     <link rel="icon" href="assets/images/logoSanAntonio.png">
 
     <!-- Style Sheets -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link rel="stylesheet" href="assets/css/navbar/style.css">
     <link rel="stylesheet" href="assets/css/index/style.css">
     <link rel="stylesheet" href="assets/css/footer/style.css">
 </head>
 
-<body data-bs-theme="light">
-
+<body data-bs-theme="dark">
     <?php
-
-        if (isset($_SESSION['userID'])) {
-            include("sharedAssets/navbarLoggedIn.php");
-        } else {
-            include("sharedAssets/navbar.php");
-        }
-
+    if (isset($_SESSION['userID'])) {
+        include("sharedAssets/navbarLoggedIn.php");
+    } else {
+        include("sharedAssets/navbar.php");
+    }
     ?>
 
-    <div class="container pt-3 mx-auto">
-
-        <div class="row">
-            <div class="col">
-                <div class="card p-5 border-0 rounded-4" style="background-color: #19AFA5;">
-                    <div class="h1 text-center m-3 fw-bold text-white">
+    <div class="container">
+        <div class="col">
+            <div class="row">
+                <div class="card mt-4 border-0">
+                    <div class="h1 text-center m-3 fw-bold">
                         Barangay San Antonio E-Services
                     </div>
-                    <div class="h5 text-center m-3 text-white">
+                    <div class="h6 text-center m-3">
                         "Makabagong Putol, Makikinabang All"
                     </div>
-                    <div class="d-flex justify-content-center mt-3">
-                        <a href="documents.php">
-                            <button class="btn btn-primary viewButton p-2">View Our Services</button>
-                        </a>
+                    <div class="d-flex justify-content-center m-4">
+                        <button class="btn btn-primary viewButton p-2"
+                            onclick="window.location.href='documents.php'">View Our Services</button>
                     </div>
                 </div>
             </div>
         </div>
+    </div>
 
-        <div class="row pt-4">
+    <div class="container mt-5 mb-5">
+        <div class="row justify-content-center">
             <div class="col">
-                <div id="carouselExampleCaptions" class="carousel slide shadow rounded-4 overflow-hidden">
-                    <div class="carousel-indicators">
-                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active"
-                            aria-current="true" aria-label="Slide 1"></button>
-                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1"
-                            aria-label="Slide 2"></button>
-                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2"
-                            aria-label="Slide 3"></button>
+                <div class="card border-0">
+                    <div id="carouselExample" class="carousel slide">
+                        <div class="carousel-inner rounded-4" style="height: 600px;">
+                            <?php
+                            $query = "SELECT image FROM announcements ORDER BY dateTime DESC";
+                            $result = mysqli_query($conn, $query);
+                            $isFirst = true;
+                            while ($row = mysqli_fetch_assoc($result)) {
+                                $image = htmlspecialchars($row['image']);
+                                $imagePath = "assets/images/announcements/" . $image;
+                                ?>
+                                <div class="carousel-item <?= $isFirst ? 'active' : '' ?>">
+                                    <img src="<?= $imagePath ?>" class="d-block w-100 h-100 object-fit-cover"
+                                        alt="Announcement Image">
+                                </div>
+                                <?php
+                                $isFirst = false;
+                            }
+                            ?>
+                        </div>
+                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample"
+                            data-bs-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Previous</span>
+                        </button>
+                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExample"
+                            data-bs-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Next</span>
+                        </button>
                     </div>
-                    <div class="carousel-inner ratio ratio-16x9">
-                        <div class="carousel-item active">
-                            <img src="assets/images/announcements/announcement3.jpg" class="d-block w-100 h-100" style="object-fit: cover; object-position: center;" alt="...">
-                        </div>
-                        <div class="carousel-item">
-                            <img src="assets/images/announcements/announcement2.jpg" class="d-block w-100 h-100" style="object-fit: cover;" alt="...">
-                        </div>
-                        <div class="carousel-item">
-                            <img src="assets/images/announcements/announcement1.jpg" class="d-block w-100 h-100" style="object-fit: cover;" alt="...">
-                        </div>
-                    </div>
-                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions"
-                        data-bs-slide="prev">
-                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden">Previous</span>
-                    </button>
-                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions"
-                        data-bs-slide="next">
-                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden">Next</span>
-                    </button>
                 </div>
             </div>
         </div>
+    </div>
 
-
-        <div class="row pt-5 p-0">
-
-            <div class="d-flex align-items-center">
-                <h3 class="me-2 mb-0">Announcements</h3>
-                <span class="badge rounded-pill text-bg-danger">New</span>
-            </div>
-
-            <div class="col-lg-4 col-12 d-flex justify-content-center">
-                <div class="card mt-3">
-                    <img src="assets/images/announcements/announcement1.jpg" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title custom-title">Announcement 1</h5>
-                        <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum magni fuga voluptatibus! Labore, similique! Sit qui, adipisci blanditiis, hic molestias illum molestiae maxime ab quia quod exercitationem? Quia, voluptas nihil.
-                        </p>
-                    </div>
+    <div class="container mb-5">
+        <div class="row mb-3">
+            <div class="col">
+                <div class="d-flex align-items-center">
+                    <h1 class="me-2 mb-0">Announcements</h1>
+                    <span class="badge rounded-pill text-bg-danger">New</span>
                 </div>
             </div>
-            <div class="col-lg-4 col-12 d-flex justify-content-center">
-                <div class="card mt-3">
-                    <img src="assets/images/announcements/announcement1.jpg" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title custom-title">Announcement 2</h5>
-                        <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum magni fuga voluptatibus! Labore, similique! Sit qui, adipisci blanditiis, hic molestias illum molestiae maxime ab quia quod exercitationem? Quia, voluptas nihil.
-                        </p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4 col-12 d-flex justify-content-center">
-                <div class="card mt-3">
-                    <img src="assets/images/announcements/announcement1.jpg" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title custom-title">Announcement 3</h5>
-                        <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum magni fuga voluptatibus! Labore, similique! Sit qui, adipisci blanditiis, hic molestias illum molestiae maxime ab quia quod exercitationem? Quia, voluptas nihil.
-                        </p>
-                    </div>
-                </div>
-            </div>
-            
         </div>
+        <div class="row">
+            <?php
+            $query = "SELECT * FROM announcements WHERE isImportant = 'TRUE' ORDER BY dateTime DESC";
+            $result = mysqli_query($conn, $query);
 
+            while ($row = mysqli_fetch_assoc($result)) {
+                $title = htmlspecialchars($row['title']);
+                $description = htmlspecialchars($row['description']);
+                $image = htmlspecialchars($row['image']);
+                $imagePath = "assets/images/announcements/" . $image;
+                ?>
+                <div class="col-md-4">
+                    <div class="card mt-3" style="width: 100%;">
+                        <img src="<?= $imagePath ?>" class="card-img-top" alt="Announcement Image">
+                        <div class="card-body">
+                            <h5 class="card-title custom-title"><?= $title ?></h5>
+                            <p class="card-text"><?= $description ?></p>
+                        </div>
+                    </div>
+                </div>
+                <?php
+            }
+            ?>
+        </div>
     </div>
 
     <?php include("sharedAssets/footer.php") ?>
