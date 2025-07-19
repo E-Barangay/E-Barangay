a38bc980114d74a184245d06c0b25cb0bfdfa5ee
le1nad-prog <melitantedaniel1215@gmail.com>
2025-07-19 23:54:56 +0800

    Adjusted the view document layout

--- documentContent/barangayHallDocuments.php
+++ documentContent/barangayHallDocuments.php
@@ -8,7 +8,7 @@ while ($barangayHallDocumentsRow = mysqli_fetch_assoc($barangayHallDocumentsResu
             <img src="assets/images/documents/<?php echo $barangayHallDocumentsRow['documentImage'] ?>" class="card-img-top"
                 style="width: 100%; height: 500px; object-fit: cover;" alt="...">
             <div class="mt-auto">
-                <a href="documentContent/document.php?documentTypeID=<?php echo $barangayHallDocumentsRow['documentTypeID'] ?>">
+                <a href="documentContent/documentView.php?documentTypeID=<?php echo $barangayHallDocumentsRow['documentTypeID'] ?>">
                     <button type="button" class="btn btn-primary documentButton mt-2">
                         <?php echo $barangayHallDocumentsRow['documentName'] ?>
                     </button>
