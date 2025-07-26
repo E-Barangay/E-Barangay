c57c8d9320000a49639bab4a3e059df488e30b45
le1nad-prog <melitantedaniel1215@gmail.com>
2025-07-26 23:34:14 +0800

    Created Search Functionality for Document Page

--- documents.php
+++ documents.php
@@ -37,13 +37,20 @@ if (isset($_GET['content'])) {
 $userQuery = "SELECT * FROM users LEFT JOIN userInfo ON users.userID = userInfo.userID";
 $userResult = executeQuery($userQuery);
 
-$allDocumentsQuery = "SELECT * FROM documentTypes ORDER BY documentName ASC";
-$allDocumentsResult = executeQuery($allDocumentsQuery);
+$searchTerm = isset($_GET['search']) ? mysqli_real_escape_string($conn, $_GET['search']) : '';
 
+$allDocumentsQuery = "SELECT * FROM documentTypes";
 $barangayHallDocumentsQuery = "SELECT * FROM documentTypes WHERE categoryID = 1";
-$barangayHallDocumentsResult = executeQuery($barangayHallDocumentsQuery);
-
 $mioDocumentsQuery = "SELECT * FROM documentTypes WHERE categoryID = 2";
+
+if (!empty($searchTerm)) {
+    $allDocumentsQuery .= " WHERE documentName LIKE '%$searchTerm%'";
+    $barangayHallDocumentsQuery .= " AND documentName LIKE '%$searchTerm%'";
+    $mioDocumentsQuery .= " AND documentName LIKE '%$searchTerm%'";
+}
+
+$allDocumentsResult = executeQuery($allDocumentsQuery);
+$barangayHallDocumentsResult = executeQuery($barangayHallDocumentsQuery);
 $mioDocumentsResult = executeQuery($mioDocumentsQuery);
 
 ?>
@@ -60,8 +67,7 @@ $mioDocumentsResult = executeQuery($mioDocumentsQuery);
     <link rel="icon" href="assets/images/logoSanAntonio.png">
 
     <!-- Style Sheets -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link rel="stylesheet" href="assets/css/navbar/style.css">
     <link rel="stylesheet" href="assets/css/documents/style.css">
@@ -81,22 +87,23 @@ $mioDocumentsResult = executeQuery($mioDocumentsQuery);
     ?>
 
     <div class="container pt-3">
-        <form method="GET">
-            <div class="row">
-                <div class="col-12 col-lg-3 p-0">
+        <div class="row">
+            <div class="col-12 col-lg-3 p-0">
 
-                    <!-- Pop Up when screen is in small size -->
+                <!-- Pop up when screen is in small size -->
+
+                <form method="GET">
 
                     <!-- Search Bar -->
                     <div class="searchBarPop position-relative d-block d-sm-none mb-4 mx-1">
-                        <input class="form-control rounded-pill ps-5" name="search" type="search" placeholder="Search Documents">
+                        <input type="hidden" name="content" value="<?php echo $content; ?>">
+                        <input class="form-control rounded-pill ps-5" name="search" type="search" placeholder="Search Documents" value="<?php echo $searchTerm ?>">
                         <i class="fa-solid fa-magnifying-glass searchIcon text-muted"></i>
                     </div>
 
                     <!-- Filter Button -->
                     <div class="d-block d-sm-none dropdown">
-                        <button class="filterButtonPop dropdown-toggle" data-bs-toggle="dropdown"
-                            aria-expanded="false"><i class="fa-solid fa-filter"></i></button>
+                        <button class="filterButtonPop dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false"><i class="fa-solid fa-filter"></i></button>
                         <ul class="dropdown-menu dropdown-menu-end">
                             <li><a class="dropdown-item <?php echo ($content == 'allDocuments') ? 'active' : ''; ?>" href="?content=allDocuments">All Documents</a></li>
                             <li><a class="dropdown-item <?php echo ($content == 'barangayHallDocuments') ? 'active' : ''; ?>" href="?content=barangayHallDocuments">Barangay Hall</a></li>
@@ -105,11 +112,18 @@ $mioDocumentsResult = executeQuery($mioDocumentsQuery);
                         </ul>
                     </div>
 
+                </form>
+
+                <!-- Pop up when screen is in big size -->
+
+                <form method="GET">
+
                     <div class="filterCard card m-1 p-2 d-none d-sm-block">
                         <div class="row">
                             <div class="col m-2">
                                 <div class="position-relative">
-                                    <input class="form-control rounded-pill ps-5" name="search" type="search" placeholder="Search Documents">
+                                    <input type="hidden" name="content" value="<?php echo $content; ?>">
+                                    <input class="form-control rounded-pill ps-5" name="search" type="search" placeholder="Search Documents" value="<?php echo $searchTerm ?>">
                                     <i class="fa-solid fa-magnifying-glass searchIcon text-muted"></i>
                                 </div>
                             </div>
@@ -130,19 +144,21 @@ $mioDocumentsResult = executeQuery($mioDocumentsQuery);
 
                         </div>
                     </div>
-                </div>
 
-                <div class="col-12 col-lg-9 p-0">
-                    <div class="contentCard card m-1 p-2">
-                        <div class="row px-3 py-2" id="scrollable" style="max-height: 100vh; overflow-y: auto;">
+                </form>
+            </div>
 
-                            <?php include("documentContent/" . $content . ".php"); ?>
+            <div class="col-12 col-lg-9 p-0">
+                <div class="contentCard card m-1 p-2">
+                    <div class="row px-3 py-2" id="scrollable" style="max-height: 100vh; overflow-y: auto;">
 
-                        </div>
+                        <?php include("documentContent/" . $content . ".php"); ?>
+
                     </div>
                 </div>
             </div>
-        </form>
+            
+        </div>
     </div>
 
     <?php include("sharedAssets/footer.php") ?>
