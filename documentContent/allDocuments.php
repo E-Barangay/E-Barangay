157dd86ffeaaef93ede0c0a3870271a936cabaa3
le1nad-prog <melitantedaniel1215@gmail.com>
2025-09-28 23:39:29 +0800

    Added modal for each documents

--- documentContent/allDocuments.php
+++ documentContent/allDocuments.php
@@ -1,18 +1,20 @@
 <?php
 
-while ($allDocumentsRow = mysqli_fetch_assoc($allDocumentsResult)) {
+while ($documentsRow = mysqli_fetch_assoc($documentsResult)) {
     ?>
 
     <div class="col-6 col-md-4 col-lg-4 p-1">
         <div class="documentCard card my-2">
-            <img src="assets/images/documents/<?php echo $allDocumentsRow['documentImage'] ?>" class="card-img-top"
+            <img src="assets/images/documents/<?php echo $documentsRow['documentImage'] ?>" class="card-img-top"
                 style="width: 100%; height: 500px; object-fit: cover;" alt="...">
             <div class="mt-auto">
-                <a href="documents/documentView.php?documentTypeID=<?php echo $allDocumentsRow['documentTypeID'] ?>">
-                    <button type="button" class="btn btn-primary documentButton mt-2">
-                        <?php echo $allDocumentsRow['documentName'] ?>
-                    </button>
-                </a>
+                <button class="btn btn-primary" type="button" data-bs-toggle="modal" data-bs-target="#barangayClearanceModal">
+                    <a href="documents/documentView.php?documentTypeID=<?php echo $documentsRow['documentTypeID'] ?>">
+                        <button type="button" class="btn btn-primary documentButton mt-2">
+                            <?php echo $documentsRow['documentName'] ?>
+                        </button>
+                    </a>
+                </button>
             </div>
         </div>
     </div>
